eb6776251b6d5f1357bf116fd6c8bd7cf64c52b7
Sopiro <knwer782@naver.com>
2021-02-02 19:33:37 +0900

    Add project files.

---
+++ GameServer/Client.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Client
+    {
+        public static int dataBufferSize = 4096;
+        public int id;
+        public TCP tcp;
+
+        public Client(int clinetId)
+        {
+            id = clinetId;
+            tcp = new TCP(id);
+        }
+
+        public class TCP
+        {
+            public TcpClient socket = null;
+
+            private readonly int id;
+
+            private NetworkStream stream;
+            private byte[] receiveBuffer;
+
+            public TCP(int id)
+            {
+                this.id = id;
+            }
+
+            public void Connect(TcpClient socket)
+            {
+                socket.ReceiveBufferSize = dataBufferSize;
+                socket.SendBufferSize = dataBufferSize;
+                  
+                receiveBuffer = new byte[dataBufferSize];
+
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+            }
+
+            private void ReceiveCallback(IAsyncResult ar)
+            {
+                try
+                {
+                    int byteLen = stream.EndRead(ar);
+                    if(byteLen <= 0)
+                    {
+                        return;
+                    }
+
+                    byte[] data = new byte[byteLen];
+                    Array.Copy(receiveBuffer, data, byteLen);
+
+                    stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error reveiving TCP data:{e}");
+                }
+            }
+        }
+    }
+}
---
+++ GameServer/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Server
+    {
+        public static int MaxPlayers { get; private set; }
+        public static int Port { get; private set; }
+        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+
+        private static TcpListener tcpListener;
+
+        public static void Start(int maxPlayers, int port)
+        {
+            MaxPlayers = maxPlayers;
+            Port = port;
+
+            Console.WriteLine("Starting server...");
+
+            InitServerData();
+
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            Console.WriteLine($"Server started on port: {Port}.");
+        }
+
+        private static void TCPConnectCallback(IAsyncResult ar)
+        {
+            TcpClient client = tcpListener.EndAcceptTcpClient(ar);
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+            Console.WriteLine($"Incomiing connection from {client.Client.RemoteEndPoint} ...");
+
+            for (int i = 1; i <= MaxPlayers; i++)
+            {
+                if (clients[i].tcp.socket == null)
+                {
+                    clients[i].tcp.Connect(client);
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{client.Client.RemoteEndPoint} failed to connet: Server full!");
+        }
+        private static void InitServerData()
+        {
+            for (int i = 1; i <= MaxPlayers; i++)
+            {
+                clients.Add(i, new Client(i));
+            }
+        }
+    }
+}
